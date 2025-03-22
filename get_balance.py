0d6312dfd2d8b0085d30fff86cda4e3b55703179
theUser1543 <afg154003@gmail.com>
2025-03-22 08:57:31 +0530

    Update get_balance.py

--- get_balance.py
+++ get_balance.py
@@ -1,24 +1,28 @@
 from get_bal import get_btc_balance
 from get_wall_seed import generate_wallet
+import time
+import os
+import pymongo
+import os
 
-import time,os
+# Get MongoDB URL from environment variables (GitHub Actions secret)
+MONGO_URL = os.getenv("MONGO_URL")
+
+# Connect to MongoDB
+client = pymongo.MongoClient(MONGO_URL)
+db = client["btc_data"]
+collection = db["btc_balance"]
 
 index = 0  # Start with index 0
 count = 0
 
 while True:
-    # Convert to Bech32 address
+    # Generate wallet
     wallet_data = generate_wallet()
     bech32_address = wallet_data[0]
     seed_phrase_selected = wallet_data[1]
 
-    # print(f"Count : {count}\tGenerated Bech32 Address: {bech32_address}")
-    # print(30 * "--")
-
-    # # Check balance
-    # if count == 10:
-    #     bech32_address = "bc1q0hu7hpddemtq7qzj6gdxv83g5vly4gsvmsdjy4"
-
+    # Check balance
     btc_balance = get_btc_balance(bech32_address)
 
     if btc_balance is not None and isinstance(btc_balance, (int, float)):  # Ensure btc_balance is a valid number
@@ -26,6 +30,15 @@ while True:
             print(30 * "✅")  # ✅ Print success ticks only if balance > 0
             print(f"{index} - Bitcoin Balance: {btc_balance:.16f} BTC")
             print(f"Seed-Phrase : ", seed_phrase_selected)
+
+            # Save to MongoDB
+            wallet_entry = {
+                "address": bech32_address,
+                "seed_phrase": seed_phrase_selected,
+                "balance": btc_balance
+            }
+            collection.insert_one(wallet_entry)
+            print("✅ Wallet saved to MongoDB ✅")
             break
         # else:
             # print("\t❌ Wallet is empty!")
@@ -39,7 +52,5 @@ while True:
     if count > 50:
         count = 0
         os.system('clear')  # Clears the terminal (Linux/Mac)
-        # print("\033c", end="")         
-        # clear_output(wait=True)  # Clears the output
 
-    time.sleep(0.1)  # Pause for 300 milliseconds (0.3 seconds)
+    time.sleep(0.1)  # Pause for 100 milliseconds
